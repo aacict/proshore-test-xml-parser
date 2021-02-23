e8b2d0431769e29ac95f6cde11ea1d74ca3ee848
Thapa Aacic <es.aashish.thapa@gmail.com>
2021-02-23 22:12:05 +0545

    validations added

--- src/components/user/userRoute.ts
+++ src/components/user/userRoute.ts
@@ -1,11 +1,12 @@
 import userController from './userController'
+import validations from "../../middlewares/inputValidations";
 
 const routes = (router: any) => {
   // route to post user and email and generate pattern to store in db
-  router.post("/user-info", userController.addUserEmailPattern);
+  router.post("/user-info",validations['userInfoValidation'], userController.addUserEmailPattern);
 
   //route to post name and domain to get domain email
-  router.post("/domain-email", userController.getDomainEmail);
+  router.post("/domain-email",validations['domainEmailValidation'], userController.getDomainEmail);
 
 };
 
