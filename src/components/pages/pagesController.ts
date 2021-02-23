8a6da3abb5e08a725504e37b9dc38b47dbe23428
Thapa Aacic <es.aashish.thapa@gmail.com>
2021-02-23 17:26:36 +0545

    comments added

--- src/components/pages/pagesController.ts
+++ src/components/pages/pagesController.ts
@@ -1,8 +1,11 @@
 
 const services = {
+  //send user info page to the view
     getNameEmailForm: (req, res, next)=> {
       res.render('userPage',{page:'user-info'});
     },
+
+  //send domain info page to the view
     getNameDomainForm: (req, res, next)=> {
       res.render('userPage',{page:'domain-email'});
     }
