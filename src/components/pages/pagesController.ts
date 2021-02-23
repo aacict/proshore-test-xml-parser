30073e5d6e7def595f19976581a3e7e064d3af34
Thapa Aacic <es.aashish.thapa@gmail.com>
2021-02-23 22:11:39 +0545

    page controller old value errors

--- src/components/pages/pagesController.ts
+++ src/components/pages/pagesController.ts
@@ -1,14 +1,14 @@
 
-const services = {
+const pageControllers = {
   //send user info page to the view
     getNameEmailForm: (req, res, next)=> {
-      res.render('userPage',{page:'user-info'});
+      res.render('userPage',{page:'user-info', errors:{}, oldValue:{}});
     },
 
   //send domain info page to the view
     getNameDomainForm: (req, res, next)=> {
-      res.render('userPage',{page:'domain-email'});
+      res.render('userPage',{page:'domain-email', errors:{},  oldValue: {}});
     }
 }
 
-export default services;
+export default pageControllers;
