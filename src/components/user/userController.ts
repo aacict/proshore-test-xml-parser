058c267b0b027c0487a700d71e40e17299468b55
Thapa Aacic <es.aashish.thapa@gmail.com>
2021-02-23 17:26:48 +0545

    regex change

--- src/components/user/userController.ts
+++ src/components/user/userController.ts
@@ -1,5 +1,6 @@
 import { EmailPattern } from "../../models";
 
+//function to take sub names from full name provided
 const splitSubnamesFromFullname = async(fullname) => {
   const full_name: any = fullname.trim().split(' ');
   const first_name: string = full_name[0];
@@ -8,25 +9,29 @@ const splitSubnamesFromFullname = async(fullname) => {
   let middle_name: string = '' ;
    if(full_name.length >= 3){
    for (let index = 1; index < full_name.length-1; index++) {
-     middle_name = middle_name + `${full_name[index]} `;
+     middle_name = middle_name + `${full_name[index]}`;
    }
    }
   return {first_name, middle_name, last_name}
 };
 const services = {
+  //takes user full name and splits full name into sub names and make changes to email accordingly
       addUserEmailPattern: async(req: any, res: any, next:any)=> {
         const {first_name, middle_name, last_name} = await splitSubnamesFromFullname(req.body['full_name'])
         const email_parts: string = req.body['email'].trim().split('@')
 
-        const first_name_reg:any = new RegExp(`${first_name}`,'gi');
-        const last_name_reg:any = new RegExp(`${last_name}`,'gi');
-        const mid_name_reg:any = new RegExp(`${middle_name.trim()}`,'gi');
+        //regex for matching sub names in email
+        const first_name_reg:any = new RegExp(`(?<![{\w])(${first_name})(?![\w}])`,'gi');
+        const last_name_reg:any = new RegExp(`(?<![{\w])(${last_name})(?![\w}])`,'gi');
+        const mid_name_reg:any = new RegExp(`(?<![{\w])(${middle_name})(?![\w}])`,'gi');
         
+        //generating pattern and attach domain to the pattern
         let email_pattern:string = email_parts[0].replace(first_name_reg,'{fn}')
         email_pattern =  middle_name ? email_pattern.replace(mid_name_reg,'{mn}') : email_pattern;
         email_pattern = last_name ? email_pattern.replace(last_name_reg,'{ln}') : email_pattern;
         email_pattern += `@${email_parts[1].toLowerCase()}`;
 
+        //find in db and save if not exits or increase count
         const emailPattern = await EmailPattern.findOne({email_pattern});
        if(emailPattern){
          emailPattern['count']++;
@@ -37,16 +42,22 @@ const services = {
 
        return res.send(`successfully loaded to db`);
       },
+
+      //takes user full name and splits full name into sub names and takes highest count email pattern of the domain  and attach user name as the pattern
       getDomainEmail: async(req: any, res: any, next:any)=> {
         const {first_name, middle_name, last_name} = await splitSubnamesFromFullname(req.body['full_name'])
         const domain: string = `@${req.body['domain'].trim()}`;
+
+        //find the domain and sort with max count
         const email_pattern: any = await EmailPattern.findOne({email_pattern: {$regex: domain, $options: 'i'}}).sort({count : -1}).limit(1);
         if(!email_pattern){
-          return res.send('no pattern avaiable for the domain')
+          return res.send('no pattern available for the domain')
         }
+
+        //change pattern to user email
         let user_email:string = email_pattern['email_pattern'].replace(/{fn}/g, first_name)
-        user_email = middle_name ? user_email.replace(/{mn}/g,  middle_name.trim()) : user_email;
-        user_email = last_name ? user_email.replace(/{ln}/g,last_name) : user_email;
+        user_email = middle_name ? user_email.replace(/{mn}/g, middle_name) : user_email.replace(/{mn}/g,  '');
+        user_email = last_name ? user_email.replace(/{ln}/g, last_name) : user_email.replace(/{ln}/g,  '');
         return res.send(user_email);
       }
 }
