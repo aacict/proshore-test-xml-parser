5a91807355b7741620ed03a312e92844a070707b
Thapa Aacic <es.aashish.thapa@gmail.com>
2021-04-01 20:08:23 +0545

    file validation added

--- src/middlewares/inputValidations.ts
+++ src/middlewares/inputValidations.ts
@@ -1,6 +1,17 @@
-
+import Model from "../models";
 const validations = {
-
+   fileValidation: async(req, res, next) => {
+    try {
+      const fileFormats = ['xlsx', 'xls', 'csv']
+      const fileFormat = req.files['file'].name.split('.').pop();
+      if(!fileFormats.includes(fileFormat)){
+          return res.send('This format is not allowed')
+      }
+      next();
+    } catch (error) {
+      console.log(error)
+    }
+   }
   }
   
   export default validations;
