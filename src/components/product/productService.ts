dbc610a9d22a61d5b8ec540a01fb702bae50f587
Thapa Aacic <es.aashish.thapa@gmail.com>
2021-04-01 20:08:12 +0545

    product service created

---
+++ src/components/product/productService.ts
@@ -0,0 +1,29 @@
+import Model from "../../models";
+const pageService = {
+
+    getProducts: async(req: any, res: any, next: any)=> {
+      // const dates = req.query['filter'] ?  req.query['filter'].split('-'): '';
+      const limit = req.query['limit'] && req.query['limit'] != 0 ? parseInt(req.query['limit'], 10) : 10;
+      const page = req.query['page'] && req.query['page'] != 0 ? parseInt(req.query['page'], 10) : 1;
+      const offset = (page - 1) * limit;
+
+      const products = await Model.Product.findAll({limit ,offset});
+      const total_products = await Model.Product.count();
+      const responseProducts = await Promise.all(
+        products.map(async(product)=>{
+            const tempProduct = JSON.parse(JSON.stringify(product))
+            const order = await Model.Order.findOne({where:{id: product.order_id}});
+            tempProduct['order_number'] = order['order_number']
+            return tempProduct;
+        })
+      )
+     return {
+        products: responseProducts,
+        page,
+        limit,
+        total_products,
+      }
+    }
+}
+
+export default pageService;
