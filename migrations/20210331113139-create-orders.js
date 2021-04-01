b98c2d554744c32de85d16843c85e3c0d9104798
Thapa Aacic <es.aashish.thapa@gmail.com>
2021-04-01 18:17:34 +0545

    migration update

--- migrations/20210331113139-create-orders.js
+++ migrations/20210331113139-create-orders.js
@@ -2,7 +2,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('orders', {
-      order_id: {
+      id: {
         primaryKey: true,
         type: Sequelize.INTEGER,
         allowNull: false,
--- migrations/20210331113221-create-products.js
+++ migrations/20210331113221-create-products.js
@@ -2,23 +2,23 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('products', {
-     product_id:{
+     id:{
         primaryKey: true,
         type: Sequelize.INTEGER.UNSIGNED,
         allowNull: false,
-        autoIncrement: false,
+        autoIncrement: true,
         unique: true,
     },
     order_id: {
         type: Sequelize.INTEGER.UNSIGNED,
      },
-    name: {type: Sequelize.STRING,},
+    product: {type: Sequelize.STRING,},
     brand: {type: Sequelize.STRING,},
     rate: {type: Sequelize.STRING,},
     quantity: {type: Sequelize.INTEGER,},
     gross_amount: {type: Sequelize.FLOAT},
     location: {type: Sequelize.STRING},
-    order_date: {type: Sequelize.DATE},
+    date: {type: Sequelize.DATE},
     });
   },
   down: async (queryInterface, Sequelize) => {
