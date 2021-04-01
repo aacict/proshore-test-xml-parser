d24b11e13443fd7389a473550af6ea4e62e83f1a
Thapa Aacic <es.aashish.thapa@gmail.com>
2021-04-01 20:07:07 +0545

    route  update

--- src/components/File/fileRoute.ts
+++ src/components/File/fileRoute.ts
@@ -2,8 +2,8 @@ import fileController from './fileController'
 import validations from "../../middlewares/inputValidations";
 
 const routes = (router: any) => {
-  router.post("/upload", fileController.uploadCsvFile);
-  router.post("/download-filter/product", fileController.downloadFilterFile);
+  router.post("/upload", validations['fileValidation'] ,fileController.uploadCsvFile);
+  router.post("/download", fileController.downloadFilterFile);
 };
 
 export default routes;
